0dd734fb0813c92f414228e67b95e98d4692ea91
alonb <regev1993@windowslive.com>
2026-01-28 20:28:45 +0200

    first commit

---
+++ playwright_training/oop_vehicles/main_vehicles.py
@@ -0,0 +1,6 @@
+from playwright_training.oop_vehicles.car_object import carObject
+from playwright_training.oop_vehicles.truck_object import TruckObject
+
+car_1 = carObject("Toyota",True)
+car_2 = carObject("Mazda",False)
+truck_1 = TruckObject("Volvo",8)
